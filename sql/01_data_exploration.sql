670ab530ad227f5c24d71cf4fded2fda93a0082d
Ron <paramironel012@gmail.com>
2026-09-14 22:36:05 +0800

    Create 01_data_exploration.sql

---
+++ sql/01_data_exploration.sql
@@ -0,0 +1,9 @@
+-- Project: Retail Sales Intelligence
+-- File: 01_data_exploration.sql
+-- Purpose: Initial data exploration and data quality checks
+
+USE RetailSalesDB;
+GO
+
+SELECT TOP 100 *
+FROM Sales;
